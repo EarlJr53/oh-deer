91e6b79d5a36d640abeae46afb0bceea04056de7
Brooke Moss <me@brookem.dev>
2024-11-14 15:53:43 -0500

    updates piezo test

--- tests/piezo_test.py
+++ tests/piezo_test.py
@@ -5,10 +5,10 @@ import pwmio
 piezo = pwmio.PWMOut(board.D5, duty_cycle=0, frequency=440, variable_frequency=True)
 
 while True:
-    for f in (440, 1000, 10000, 20000, 30000, 40000):
+    for f in (10000, 20000):
         piezo.frequency = f
         piezo.duty_cycle = 65535 // 2  # On 50%
-        time.sleep(0.25)  # On for 1/4 second
+        time.sleep(0.5)  # On for 1/4 second
         piezo.duty_cycle = 0  # Off
-        time.sleep(0.05)  # Pause between notes
-    time.sleep(0.5)
+        time.sleep(0.25)  # Pause between notes
+    # time.sleep(0.5)

322ac032aa1f61690fa5cfd763597683ebd8f8cf
Brooke Moss <me@brookem.dev>
2024-11-19 19:24:44 -0500

    adds keyboardinterrupt catch

--- main.py
+++ main.py
@@ -2,42 +2,53 @@ from deterrent_utils import Servo, Ultrasonic
 from thermal_utils import Thermal
 import time
 
-lock = False        # Whether the device has detected a deer
-start_time = time.time() # time at last detection
-dwell_time = 0      # number of seconds since last detection
-max_dwell = 20     # max time to wait since last detection before sleep
 
-servo = Servo(pid_enable=False)
-ultrasonic = Ultrasonic()
-thermal = Thermal()
+def main():
+    try:
+        lock = False  # Whether the device has detected a deer
+        start_time = time.time()  # time at last detection
+        dwell_time = 0  # number of seconds since last detection
+        max_dwell = 20  # max time to wait since last detection before sleep
 
-while lock or (dwell_time < max_dwell):
+        servo = Servo(pid_enable=False)
+        ultrasonic = Ultrasonic()
+        thermal = Thermal()
 
-    # Call thermal camera object to see if there is a deer in the frame
-    # Return in the form (lock, target)
-    # Where lock is boolean expressing whether there is a deer
-    # and target is an integer for the horizotal pixel that aligns with the deer
+        while lock or (dwell_time < max_dwell):
 
-    lock, target = thermal.detect()   # it should look like this
-    # target = 40                         # temporary until the thermal camera is integrated
+            # Call thermal camera object to see if there is a deer in the frame
+            # Return in the form (lock, target)
+            # Where lock is boolean expressing whether there is a deer
+            # and target is an integer for the horizotal pixel that aligns with the deer
 
-    if lock:
-        servo.track(target)
-        # ultrasonic.random()
-        start_time = time.time()
-        print(target)
+            lock, target = thermal.detect()  # it should look like this
+            # target = 40                         # temporary until the thermal camera is integrated
 
-    else:
+            if lock:
+                servo.track(target)
+                # ultrasonic.random()
+                start_time = time.time()
+                print(target)
+
+            else:
+                ultrasonic.off()
+                # servo.idle()
+                servo.off()
+                print("Idle")
+
+            dwell_time = time.time() - start_time
+
+    except KeyboardInterrupt:
+        print("\nExited by user")
         ultrasonic.off()
-        #servo.idle()
         servo.off()
-        print("Idle")
+        thermal.off()
 
-    dwell_time = time.time() - start_time
+    finally:
+        print("\nExited naturally")
+        ultrasonic.off()
+        servo.off()
+        thermal.off()
 
 
-ultrasonic.off()
-servo.off()
-thermal.off()
-
-    
+main()

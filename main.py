9f842ff174b0b6853c65f4274483d73855af816e
Brooke Moss <me@brookem.dev>
2024-12-05 18:20:46 -0500

    add persistent operating mode

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 from deterrent_utils import Servo, Ultrasonic
 from thermal_utils import Thermal
 import time
+import logging
 
+logger = logging.getLogger(__name__)
 
 def main():
     try:
         lock = False  # Whether the device has detected a deer
         start_time = time.time()  # time at last detection
         dwell_time = 0  # number of seconds since last detection
-        max_dwell = 20  # max time to wait since last detection before sleep
+        max_wait = 3    # time to wait before spinning after seeing a target
+        max_spin = 20  # time to spin after last detection before sleeping
+        max_sleep = 120 # time to wait before waking from sleep
 
         servo = Servo(pid_enable=False)
         # ultrasonic = Ultrasonic()
         thermal = Thermal()
 
-        while lock or (dwell_time < max_dwell):
+        # while lock or (dwell_time < max_dwell):
+        while True:
 
             # Call thermal camera object to see if there is a deer in the frame
             # Return in the form (lock, target)
             # Where lock is boolean expressing whether there is a deer
             # and target is an integer for the horizotal pixel that aligns with the deer
 
-            lock, target = thermal.detect()  # it should look like this
-            # target = 40                         # temporary until the thermal camera is integrated
+            lock, target = thermal.detect()
 
-            if lock:
+            if lock:                        # lock onto target
                 servo.track(target)
                 # ultrasonic.random()
                 start_time = time.time()
                 print(target)
 
-            else:
-                #ultrasonic.off()
-                # servo.idle()  # make it idle after like 5 seconds of inactivity
-                servo.off()   # get rid of this for test
-                print("Idle")
+            elif dwell_time < max_wait:     # briefly wait for target to reappear
+                # ultrasonic.off()
+                servo.off()
+                print("Waiting for Target")
+
+            elif dwell_time < max_spin:     # spin to reacquire target
+                # ultrasonic.off()
+                servo.idle()
+                print("Idle Spin")
+
+            elif dwell_time < max_sleep:    # enter sleep mode
+                # ultrasonic.off()
+                servo.off()
+                print("Sleep")
+
+            else:                           # awaken from sleep
+                start_time = time.time()    # no target, but restart scan cycle
+                servo.off()
 
             dwell_time = time.time() - start_time
 
@@ -50,5 +67,5 @@ def main():
         servo.off()
         thermal.off()
 
-
-main()
+if __name__ == '__main__':
+    main()
